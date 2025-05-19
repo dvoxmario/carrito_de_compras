c3d6216b953c4b26106442ccc61063b9c03f29f4
chaverradvox <chaverradvox@gmail.com>
2025-05-18 19:36:06 -0500

    se finalizo el carrito de compras y se le agrego localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/guitarras"
 import { db } from './data/db'
 
 function App() {
 
-    const [data, setData] = useState(db)
-    const [cart, setCart] = useState([])
+
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+    }
+
+
+    const [data] = useState(db)
+    const [cart, setCart] = useState([initialCart])
+
+
+    const MAX_ITEM = 5 
+    const MIN_ITEM = 1
     
+// useEffect se usa para tener sincronizado el carrito y escribiendo localStorage 
+    useEffect(() => {
+         localStorage.setItem('cart', JSON.stringify(cart))
+
+    }, [cart])
+
     function addToCart(item) {
 
         const itemExists = cart.findIndex(guitar => guitar.id === item.id)
-        if(itemExists >=0) { // existe en el carrito 
+        if(itemExists >=0) { // existe en el carrito
+        if(cart[itemExists].quantity >= MAX_ITEM) return 
             const updatedCart = [...cart]
             updatedCart[itemExists].quantity++// sumar elementos al carrito con una copia sin mutar el state
             setCart(updatedCart)
@@ -22,14 +40,61 @@ function App() {
             setCart([...cart, item])
         }
 
-    
+       
     }
+
+    function decreaseQuantity(id) {
+        const updatedCart = cart.map( item => {
+            if(item.id === id && item.quantity > MIN_ITEM) { 
+            return{
+                ...item,
+                quantity: item.quantity - 1
+                }
+            }    
+         return item 
+        })
+
+        setCart(updatedCart)
+
+    }    
+
+
+
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+
+    function increaseQuantity(id) {
+
+        const updatedCart = cart.map(item => {
+            if(item.id === id && item.quantity < MAX_ITEM) {
+                return{ 
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function clearCart() {
+
+        setCart([])
+    }
+ 
+
 
     return (
       <>
 
        <Header
            cart={cart} 
+           removeFromCart={removeFromCart}
+           decreaseQuantity={decreaseQuantity}
+           increaseQuantity={increaseQuantity}
+           clearCart={clearCart}
+           
         
        />
     
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 import { useMemo } from 'react'
 
-export default function Header({cart}) {
+export default function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart}) {
 
     //state derivado
     const isEmpty = useMemo( () => cart.length === 0, [cart])// se ejecuta solo cuando carrito ha sido modificado, render toda la app 
@@ -14,14 +14,14 @@ export default function Header({cart}) {
                     <div className="row justify-content-center justify-content-md-between">
                         <div className="col-8 col-md-3">
                              <a href="index.html">
-                                 <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                                 <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
                             </a>
                         </div>
                         <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                              <div 
                                  className="carrito"
                             >
-                                <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                                <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                                 <div id="carrito" className="bg-white p-3">
                             
@@ -60,6 +60,7 @@ export default function Header({cart}) {
                                             <button
                                                 type="button"
                                                 className="btn btn-dark"
+                                                onClick={() => decreaseQuantity(guitar.id)}
                                             >
                                                 -
                                             </button>
@@ -67,6 +68,7 @@ export default function Header({cart}) {
                                             <button
                                                 type="button"
                                                 className="btn btn-dark"
+                                                onClick={() => increaseQuantity(guitar.id)}
                                             >
                                                 +
                                             </button>
@@ -75,6 +77,7 @@ export default function Header({cart}) {
                                             <button
                                                 className="btn btn-danger"
                                                 type="button"
+                                                onClick={() => removeFromCart(guitar.id)}
                                             >
                                                 X
                                             </button>
@@ -89,7 +92,10 @@ export default function Header({cart}) {
                             </>
                             )} 
 
-                            <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            <button 
+                            className="btn btn-dark w-100 mt-3 p-2"
+                            onClick={clearCart}
+                            >Vaciar Carrito</button>
                         </div>
                     </div>
                 </nav>
